bfc0edb4b1e955c44043d22e64de9645bfb6cb76
Kainoa Seaman <kaigra@icloud.com>
2024-05-28 18:01:31 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import Quiz from './components/Quiz';
+
+function App() {
+
+    return (
+        <>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
+        </>
+    );
+}
+
+export default App;
